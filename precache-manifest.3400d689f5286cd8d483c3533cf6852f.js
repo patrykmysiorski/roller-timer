4f6226c894ba1004754cb82c9fde8518b57f7561
Patryk Mysiorski <mysior@ncdc.pl>
2020-11-09 17:27:59 +0100

    Updates

---
+++ precache-manifest.3400d689f5286cd8d483c3533cf6852f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643de564afde69a4356bc84215b878a0",
+    "url": "/roller-timer/index.html"
+  },
+  {
+    "revision": "075d6d8dab099651ea1d",
+    "url": "/roller-timer/static/css/main.7bbf699c.chunk.css"
+  },
+  {
+    "revision": "ab12cbb91e3d96b900c6",
+    "url": "/roller-timer/static/js/2.7d195ea0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/roller-timer/static/js/2.7d195ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bae44a3adfcb5dd78b6",
+    "url": "/roller-timer/static/js/3.258807d9.chunk.js"
+  },
+  {
+    "revision": "196f31064c641ef86b22f34244b7ad03",
+    "url": "/roller-timer/static/js/3.258807d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075d6d8dab099651ea1d",
+    "url": "/roller-timer/static/js/main.4f825e16.chunk.js"
+  },
+  {
+    "revision": "c3dd0a734b7ded0c6346",
+    "url": "/roller-timer/static/js/runtime-main.efcc3380.js"
+  },
+  {
+    "revision": "c5a8dcbd9f0467f2676582d072ad7a19",
+    "url": "/roller-timer/static/media/sound.c5a8dcbd.mp3"
+  }
+]);
